b70fcd3c2632ef9e616f8ab716a50a769a3f165e
Bikash Bhattarai <bikash.bhattarai@alphaventus.com>
2021-02-20 00:31:01 +0545

    Chat-app server, client

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState();
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -19,13 +21,34 @@ const Chat = ({ location }) => {
         socket.emit('join', { name, room }, () => {});
         return () => {
             socket.emit('disconnect');
-            socket.off()
+            socket.off();
         };
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if (messasge) {
+            socket.emit('sendMessage', messsage, () => setMessage(''));
+        }
+    };
+
     return (
         <div>
-            <h1>Chat</h1>
+            <div>
+                <input
+                    value={message}
+                    onChange={(event) => {
+                        setMessage(event.target.value);
+                    }}
+                    onKeyPress={(event) => (event.key === 'Enter' ? sendMessage(event) : null)}
+                />
+            </div>
         </div>
     );
 };
